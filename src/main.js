8d658bebe5a2e68d4da4fd7e10b51fbbe9453c32
Edwin Estrada <edwin@thinkpixbit.com>
2016-08-20 00:16:28 -0400

    Added 2nd component w/o routing/nav between pages

--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ var jQuery = require('jquery');
 var $ = jQuery;
 var React = require('react');
 var Home = require('./components/homePage');
+var About = require('./components/about/aboutPage');
 
 React.render(<Home />, document.getElementById('app'));
